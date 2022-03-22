19410373fadc4f304aaf2995edee433a7e6a2c62
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-03-22 11:39:15 +0530

    Add files via upload

---
+++ basic_A_chars.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print("array of the type:",type(a))
+print("No.of dimensions:",a.ndim)
+print("shape of the array:",a.shape)
+print("size of the array:",a.size)
+print("array data type is :",a.dtype)
