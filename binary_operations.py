ca02905060cf89a1f28f830e30fc26f9ef34049a
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-03-29 11:42:30 +0530

    Add files via upload

---
+++ binary_operations.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 11:34:35 2022
+
+@author: DSP-LAB
+"""
+
+import numpy as np
+a=np.array([[1,2],[3,4]])
+b=np.array([[4,3],[2,1]])
+print("array sum :\n",a+b)
+print("araay multiplication:\n",a*b)
+print("matrix multiplication:\n",a.dot(b))
+
