d5f299ff7243a7571aef84a711969203d6d1c3a4
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-03-29 11:55:31 +0530

    Add files via upload

---
+++ universal_functions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 11:42:40 2022
+
+@author: DSP-LAB
+"""
+
+import numpy as np
+a=np.array([0,np.pi/2,np.pi])
+print("sine values of array elements:",np.sin(a))
+a=np.array([0,1,2,3])
+print("exponent of array elements:",np.exp(a))
+print("square root of array elements:",np.sqrt(a))
